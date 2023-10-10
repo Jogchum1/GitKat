782ebd4199330f58939f36efe33edb22d74f82d2
Jogchum1 <jogchum1@hotmail.nl>
2023-10-10 16:35:57 +0200

    Started new grapple version

--- Snorkelkat/Assets/GrapplingHook.cs
+++ Snorkelkat/Assets/GrapplingHook.cs
@@ -6,9 +6,8 @@ public class GrapplingHook : MonoBehaviour
 {
 
     public DistanceJoint2D joint;
-    public Vector2 shootingDir;
-    private Vector2 targetLocation;
-    private bool lookingLeft = false;
+    public GameObject grappleChecker;
+    //public List<>
     // Start is called before the first frame update
     void Start()
     {
@@ -18,38 +17,18 @@ public class GrapplingHook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if(gameObject.transform.rotation.y < 0)
-        //{
-        //    lookingLeft = true;
-        //    shootingDir.x = -shootingDir.x;
-        //    targetLocation = new Vector2(shootingDir.x + gameObject.transform.position.x, shootingDir.y + gameObject.transform.position.y);
-        //}
-        //else
-        //{
-        //    lookingLeft = false;
-        //    targetLocation = new Vector2(shootingDir.x + gameObject.transform.position.x, shootingDir.y + gameObject.transform.position.y);
-        //}
         
         Grapple();
     }
 
     public void Grapple()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            targetLocation = new Vector2(shootingDir.x + gameObject.transform.position.x, shootingDir.y + gameObject.transform.position.y);
-            joint.connectedAnchor = targetLocation;
-            joint.enabled = true;
-        }
-        if (Input.GetKeyUp(KeyCode.E))
-        {
-            joint.enabled = false;
-        }
+        
         
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(gameObject.transform.position, targetLocation);
+
     }
 }
