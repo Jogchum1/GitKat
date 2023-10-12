3a1bd948f2b7f7608722b2f139aedbbe0060527d
Jogchum1 <jogchum1@hotmail.nl>
2023-10-12 11:31:41 +0200

    Variable Jump height

--- Snorkelkat/Assets/Scripts/PlayerMovement.cs
+++ Snorkelkat/Assets/Scripts/PlayerMovement.cs
@@ -69,15 +69,15 @@ public class PlayerMovement : MonoBehaviour
             }
 
             //Small jump
-            if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-                jumpBufferCount = 0;
-                Debug.Log("Jump 3");
-
-            }
         }
 
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f && jumpsLeft >= 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            jumpBufferCount = 0;
+            Debug.Log("Jump 3");
+
+        }
 
 
         //Hantime
