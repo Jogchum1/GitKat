a47130f9720efb72331af95a8c6dee8d24c81c2c
Jogchum1 <jogchum1@hotmail.nl>
2023-11-13 16:53:11 +0100

    Update LightAbility.cs

--- Snorkelkat/Assets/Scripts/LightAbility.cs
+++ Snorkelkat/Assets/Scripts/LightAbility.cs
@@ -7,25 +7,41 @@ using UnityEngine.Rendering.Universal;
 public class LightAbility : MonoBehaviour
 {
     public Light2D playerLight;
-    //public List<Light2D> fungiLights = new List<Light2D>();
+    public GameObject placeableLight;
     public GameObject envLights;
+    //public List<Light2D> fungiLights = new List<Light2D>();
+
+    private bool canPlaceLights = false;
+    private List<GameObject> Lights = new List<GameObject>();
+
     public void PlayerLight()
     {
-        playerLight.enabled = true;
+        canPlaceLights = true;
+        //playerLight.enabled = true;
         //foreach (Light2D light in fungiLights)
         //{
         //    light.enabled = false;
         //}
-        envLights.SetActive(false);
+        //envLights.SetActive(false);
     }
 
     public void FungiLight()
     {
-        playerLight.enabled = false;
+        canPlaceLights = false;
+        //playerLight.enabled = false;
         //foreach (Light2D light in fungiLights)
         //{
         //    light.enabled = true;
         //}
-        envLights.SetActive(true);
+        //envLights.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return) && canPlaceLights == true)
+        {
+            GameObject tmpLight = Instantiate(placeableLight);
+
+        }
     }
 }
