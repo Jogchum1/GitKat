3daeeabd8ec0304e954ab995c5ac79cb5206e5f9
Jogchum1 <jogchum1@hotmail.nl>
2023-12-13 12:56:58 +0100

    geen concentratie begin van paddos

---
+++ Snorkelkat/Assets/Scripts/Prototype3/Abilities/PaddoJump.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PaddoJump", menuName = "AbilitySystem/Abilities/PaddoJump")]
+public class PaddoJump : ModAbility
+{
+    public override void ActivateAbility()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void DeactivateAbility()
+    {
+        throw new System.NotImplementedException();
+    }  
+}
