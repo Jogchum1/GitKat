c9413c43b364d74c3fa970c07c2cda87d8054ff8
Jogchum1 <jogchum1@hotmail.nl>
2023-12-18 16:24:13 +0100

    Added Paddo jump

--- Snorkelkat/Assets/Scripts/Prototype3/Abilities/PaddoJump.cs
+++ Snorkelkat/Assets/Scripts/Prototype3/Abilities/PaddoJump.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PaddoJump", menuName = "AbilitySystem/Abilities/PaddoJump")]
 public class PaddoJump : ModAbility
 {
+    public GameObject paddo;
+    
     public override void ActivateAbility()
     {
-        throw new System.NotImplementedException();
+        if(gameManager.playerMovement.jumpingPaddo == null)
+        {
+            gameManager.playerMovement.jumpingPaddo = paddo;
+        }
+
+        gameManager.playerMovement.canPaddoJump = true;
     }
 
     public override void DeactivateAbility()
     {
-        throw new System.NotImplementedException();
-    }  
+        gameManager.playerMovement.canPaddoJump = false;
+    }
 }
