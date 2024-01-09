3bb35878ae3879050870679cf71f5d3ff5ee8c7d
BartPC <bartvanlierop.bvl@gmail.com>
2024-01-09 17:05:23 +0100

    Paddo doet animatie nu

--- Snorkelkat/Assets/Scripts/AbilitySystem/Paddo.cs
+++ Snorkelkat/Assets/Scripts/AbilitySystem/Paddo.cs
@@ -5,15 +5,54 @@ using UnityEngine;
 public class Paddo : MonoBehaviour
 {
     public int paddoTime = 10;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private Collider2D col;
+
     // Start is called before the first frame update
     void Start()
     {
+        //DisablePaddoCollider();
         StartCoroutine(DestoryPaddo());
     }
 
     private IEnumerator DestoryPaddo()
     {
         yield return new WaitForSeconds(paddoTime);
+        DisablePaddoCollider();
+        DespawnPaddo();
+    }
+
+    public void EnablePaddoCollider()
+    {
+        //col.isTrigger = false;
+    }  
+
+    public void DisablePaddoCollider()
+    {
+        col.isTrigger = true;
+    }
+
+    public void DestroyPaddoNow()
+    {
         Destroy(gameObject);
+    }
+
+    public void DespawnPaddo ()
+    {
+        animator.SetTrigger("Despawn");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector2 enterDirection = (collision.transform.position - col.bounds.center).normalized;
+            if (enterDirection.y > 0f)
+            {
+                animator.SetTrigger("Bounce");
+            }
+        }
     }
 }
